6eaebdc9962c26d05a11d71144cce0772e1ad25f
AYSaradelacallem <94898287+AYSaradelacallem@users.noreply.github.com>
2022-04-08 11:20:29 +0200

    8abr_5

--- contact.php
+++ contact.php
@@ -1,15 +1,11 @@
-<?php $_POST["introducir_nombre"]; 
-$_POST["introducir_email"]; 
- $_POST["repeat_email"];
- $_POST["introducir_telefono"]; 
- $name= $_POST["introducir_website"];
-$_POST["introducir_asunto"]; 
-  $_POST["introducir_mensaje"]; 
- $formcontent="From: $name \n Message: $message";
-$recipient = "info@ayeconomics.com";
+
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "emailaddress@here.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
 ?>
-
