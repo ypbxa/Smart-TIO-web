a6f385785dccf0190a5d441cbdd383599c8a7ba1
AYSaradelacallem <94898287+AYSaradelacallem@users.noreply.github.com>
2022-04-08 13:49:37 +0200

    8bril_6

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "emailaddress@here.com";
+$to = "info@ayeconomics.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
