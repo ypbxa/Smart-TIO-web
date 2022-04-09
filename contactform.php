f697c9554612a7996c7d193fee0336f42868ff0a
ypbxa <80788385+ypbxa@users.noreply.github.com>
2022-04-09 20:51:29 +0200

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,5 +1,4 @@
 <?php
-if(isset($_POST['submit'])){ 
     $name = $_POST['name']; 
     $subject = $_POST['subject']; 
     $mailFrom = $_POST['mail'];
@@ -9,7 +8,8 @@ if(isset($_POST['submit'])){
     $headers = "From: ".$mailFrom;
     $txt= "You have received an e-mail from".$name.".\n\n".$message
      
- mail($mailTo, $subject , $txt, $headers);
+ mail($mailTo, $subject , $txt, $headers)
+        or die("Error!");
  header("location: index.php?mailsend");
-}
+
 ?>
