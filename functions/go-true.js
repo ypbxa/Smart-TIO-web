d87ea7ce34dbdf11aa5ec51c447f7016624168cd
peterfur <pedro.furelosfabeiro@gmail.com>
2022-05-23 14:24:58 +0200

    changes netlify

---
+++ functions/go-true.js
@@ -0,0 +1,16 @@
+import GoTrue from 'gotrue-js';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const GoTrue = import('gotrue-js')
+import { NETLIFY_IDENTITY_URL } from './api-constants';
+// const GoTrue = require('gotrue-js')
+
+export const auth = new GoTrue({
+	APIUrl: NETLIFY_IDENTITY_URL,
+	audience: '',
+	setCookie: true
+});
+console.log(GoTrue)
+
+// GoTrue.APIUrl = NETLIFY_IDENTITY_URL
+// console.log(GoTrue)
+// export const auth = GoTrue
