904ae9290559a6173170211297b5bbb9dd98502e
peterfur <pedro.furelosfabeiro@gmail.com>
2022-07-18 14:00:38 +0200

    form2

--- form.js
+++ form.js
@@ -5,31 +5,3 @@ const urls= new URL(window.location.href);
 
 doc.value=urls.searchParams.get('q').toString();
 doc2.value= urls.searchParams.get('r').toString();
-
-/*$("3").each(function () {
-    var temp = "a";
-    var mySelect = document.getElementById('3');
-    alert("a");
-
-    for (var i, j = 0; i = mySelect.options[j]; j++) {
-        if (i.value == temp) {
-            mySelect.selectedIndex = j;
-            break;
-        }
-    }
-    function id(btn) {
-        alert(btn.id);
-    }
-}*/
-
-/*[...document.querySelectorAll("[class='3']")].forEach(function (btn) {
-    btn.addEventListener('click', function () {
-        $.get('buyingform.html', null, function (text) {
-            text.getElementById("numberUsers");
-            text.value = '2';
-            console.log("hei");
-        });
-
-
-    });
-});*/
